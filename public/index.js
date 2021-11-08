4b5b5f859145fdf8b169741c332070b60f780609
Petr Tsymbarovich <petr@tsymbarovich.ru>
2021-11-08 14:32:37 +0300

    Fetch available repositories automatically

--- public/index.js
+++ public/index.js
@@ -1,8 +1,8 @@
 'strict'
 
+let repositories  = null
 let sfos          = null
 let arch          = null
-let aarch64       = null
 let loading       = null
 let chum_link     = null
 let gui_link      = null
@@ -12,14 +12,42 @@ let no_gui_link   = null
 window.onload = async () => {
   sfos          = document.getElementById('sfos')
   arch          = document.getElementById('arch')
-  aarch64       = document.getElementById('aarch64')
   loading       = document.getElementById('loading')
   chum_link     = document.getElementById('chum-link')
   gui_link      = document.getElementById('chum-gui-link')
   no_chum_link  = document.getElementById('no-chum-link')
   no_gui_link   = document.getElementById('no-chum-gui-link')
 
-  await update()
+  await fetch_versions()
+
+  loading.hidden = true
+  document.getElementById('form').hidden = false
+}
+
+function option(v) {
+  const o = document.createElement('option')
+  o.value = v
+  o.text  = v
+  return o
+}
+
+async function fetch_versions() {
+  const repos_reply = await fetch('/.netlify/functions/lambda/repositories')
+  const maybe_repos = await repos_reply.json()
+
+  if (maybe_repos.error) {
+    alert(
+`Failed to fetch available repositories
+
+Server reply: ${maybe_repos.error}`
+    )
+    return
+  }
+
+  repositories = maybe_repos.repositories
+
+  repositories.forEach(([v]) => sfos.appendChild(option(v)))
+  sfos.selectedIndex = 0
 }
 
 function set_link(url, link, no_link) {
@@ -32,25 +60,48 @@ function set_link(url, link, no_link) {
   }
 }
 
-async function update() {
+function hide_link_elements() {
   chum_link.hidden    = true
   gui_link.hidden     = true
   no_chum_link.hidden = true
   no_gui_link.hidden  = true
+}
+
+async function onsfos() {
+  hide_link_elements()
+
+  let arch_value = arch.value
+
+  while (arch.lastChild.value !== 'none') {
+    arch.removeChild(arch.lastChild)
+  }
+
+  const archs = repositories[sfos.selectedIndex - 1][1]
+  archs.forEach(a => arch.appendChild(option(a)))
+  document.getElementById('arch-container').hidden = false
+
+  if (archs.includes(arch_value)) {
+    arch.value = arch_value
+    onarch()
+  } else {
+    arch.selectedIndex = 0
+  }
+}
+
+async function onarch() {
+  hide_link_elements()
 
   const sfos_value = sfos.value
-  aarch64.hidden = sfos_value < '4.0.1.48'
-  if (aarch64.hidden && arch.value === 'aarch64') {
-    arch.value = 'none'
-  }
   const arch_value = arch.value
-  const show_link  = sfos_value !== 'none' && arch_value !== 'none'
-  if (show_link) {
-    loading.hidden = false
-    const res = await fetch(`/.netlify/functions/lambda/${sfos.value}/${arch.value}`)
-    const {chum, gui} = await res.json()
-    loading.hidden = true
-    set_link(chum, chum_link, no_chum_link)
-    set_link(gui,  gui_link,  no_gui_link)
+
+  if (sfos_value === 'none' || arch_value === 'none') {
+    return
   }
+
+  loading.hidden = false
+  const res = await fetch(`/.netlify/functions/lambda/packages/${sfos.value}_${arch.value}`)
+  const {chum, gui} = await res.json()
+  loading.hidden = true
+  set_link(chum, chum_link, no_chum_link)
+  set_link(gui,  gui_link,  no_gui_link)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(try_trait_v2)]
 
 use std::{
+  collections::HashMap,
   convert::{Infallible, TryFrom},
   fmt::Display,
   io::prelude::*,
@@ -19,6 +20,11 @@ pub mod impl_lambda;
 
 #[cfg(feature = "server")]
 pub mod impl_server;
+
+#[derive(Serialize)]
+pub struct Repositories {
+  repositories: Vec<(String, Vec<String>)>,
+}
 
 #[derive(Serialize)]
 struct RpmUrls {
@@ -60,6 +66,12 @@ impl From<&RpmUrls> for Response {
   }
 }
 
+impl From<&Repositories> for Response {
+  fn from(repos: &Repositories) -> Self {
+    (StatusCode::OK, repos).into()
+  }
+}
+
 impl From<&str> for Response {
   fn from(error: &str) -> Self {
     Self::error500(error)
@@ -96,25 +108,29 @@ impl<B: Buf> ClientResponse<B> {
 
 struct Client {
   client:   hyper::Client<HttpsConnector<HttpConnector>, Body>,
-  repo_url: String,
+  base_url: String,
 }
 
 impl Client {
-  fn new(repo_id: &str) -> Self {
-    let repo_base = "https://repo.sailfishos.org/obs/sailfishos:/chum/";
-    let mut repo_url = String::new();
-    repo_url.reserve(repo_base.len() + repo_id.len());
-    repo_url += repo_base;
-    repo_url += repo_id;
-
+  fn new() -> Self {
     Self {
-      client: hyper::Client::builder().build(HttpsConnector::new()),
-      repo_url,
+      client:   hyper::Client::builder().build(HttpsConnector::new()),
+      base_url: "https://repo.sailfishos.org/obs/sailfishos:/chum/".into(),
     }
   }
 
+  fn new_for_repo(repo_id: &str) -> Self {
+    let mut client = Self::new();
+    client.base_url += repo_id;
+    client.base_url += "/";
+    client
+  }
+
   async fn get(&self, path: &str) -> Result<ClientResponse<impl Buf>, hyper::Error> {
-    let url  = format!("{}/{}", self.repo_url, path);
+    let mut url = String::new();
+    url.reserve(self.base_url.len() + path.len());
+    url += &self.base_url;
+    url += path;
     println!("GET {}", url);
     let uri  = hyper::Uri::try_from(url).unwrap();
     let res  = self.client.get(uri).await?;
@@ -124,7 +140,7 @@ impl Client {
 }
 
 pub struct Handler {
-  repo_cache: std::collections::HashMap<String, Repo>,
+  repo_cache: HashMap<String, Repo>,
 }
 
 impl Clone for SharedHandler {
@@ -133,27 +149,44 @@ impl Clone for SharedHandler {
   }
 }
 
+const FUNCTIONS_BASE: &'static str = "/.netlify/functions/lambda";
+
 impl Handler {
   pub fn shared() -> SharedHandler {
     SharedHandler(Arc::new(Mutex::new(Self {
-      repo_cache: std::collections::HashMap::new(),
+      repo_cache: HashMap::new(),
     })))
   }
 
-  async fn handle<T>(&mut self, req: Request<T>) -> Response<> {
-    let parts = req
-      .uri()
-      // Path without leading `/`
-      .path()[1..]
-      .split('/')
-      .collect::<Vec<&str>>();
+  async fn repositories(&mut self) -> Response<> {
+    let index = Client::new().get("")
+      .await?
+      .text();
 
-    let repo_id = match parts.len() {
-      n if n >= 2 => parts[n-2..].join("_"),
-      _ => return Response::error(StatusCode::NOT_FOUND, "Not found"),
+    let mut res = Repositories {
+      repositories: Vec::new(),
     };
 
-    let client = Client::new(&repo_id);
+    // TODO: A better way of searching IDs
+    let re = regex::Regex::new(r">(\d+\.\d+\.\d+\.\d+)_(\w+)/<").unwrap();
+    let mut current_sfos: Option<&str> = None;
+    for c in re.captures_iter(&index) {
+      let sfos = c.get(1).unwrap().as_str();
+      let arch = c.get(2).unwrap().as_str();
+      if current_sfos.is_none() || current_sfos.unwrap() != sfos {
+        res.repositories.push((sfos.into(), Vec::new()));
+        current_sfos = Some(sfos);
+      }
+      res.repositories.last_mut().unwrap().1.push(arch.into());
+    }
+
+    res.repositories.sort_by(|a, b| b.0.cmp(&a.0));
+
+    (&res).into()
+  }
+
+  async fn packages(&mut self, repo_id: &str) -> Response<> {
+    let client = Client::new_for_repo(&repo_id);
 
     let repomd = client.get("repodata/repomd.xml")
       .await?
@@ -170,7 +203,7 @@ impl Handler {
       None    => return r#""primary" data in repomd.xml has no "checksum""#.into(),
     };
 
-    let Repo{rpm_urls, ..} = match self.repo_cache.get(&repo_id) {
+    let Repo{rpm_urls, ..} = match self.repo_cache.get(repo_id) {
       Some(r) if r.checksum == checksum => r,
       _ => {
         let location = match primary.children().find(|e| e.has_tag_name("location")) {
@@ -192,7 +225,7 @@ impl Handler {
             if let Some(name) = pkg.children().find(|e| e.has_tag_name("name")) {
               let get_url = || {
                 match pkg.children().find(|e| e.has_tag_name("location") && e.has_attribute("href")) {
-                  Some(l) => format!("{}/{}", client.repo_url, l.attribute("href").unwrap()),
+                  Some(l) => format!("{}/{}", client.base_url, l.attribute("href").unwrap()),
                   None    => String::new(),
                 }
               };
@@ -211,12 +244,28 @@ impl Handler {
             gui,
           }
         };
-        self.repo_cache.insert(repo_id.clone(), repo);
-        self.repo_cache.get(&repo_id).unwrap()
+        self.repo_cache.insert(repo_id.into(), repo);
+        self.repo_cache.get(repo_id).unwrap()
       }
     };
 
     rpm_urls.into()
+  }
+
+  async fn handle<T>(&mut self, req: Request<T>) -> Response<> {
+    let path = req.uri().path();
+
+    if path.starts_with(FUNCTIONS_BASE) {
+      let path = &path[FUNCTIONS_BASE.len()..];
+      if path == "/repositories" {
+        return self.repositories().await
+      }
+      if path.starts_with("/packages") {
+        return self.packages(&path["/packages".len()..]).await
+      }
+    }
+
+    Response::error(StatusCode::NOT_FOUND, "Not found")
   }
 }
 
