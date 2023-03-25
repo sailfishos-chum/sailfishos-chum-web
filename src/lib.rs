b95fe23513fe001ffba1289441790bd17881d85d
olf <Olf0@users.noreply.github.com>
2023-03-25 05:06:09 +0100

    Beautify, as before

--- src/lib.rs
+++ src/lib.rs
@@ -231,7 +231,7 @@ impl Handler {
               };
               match name.text() {
                 Some(n) if n == "sailfishos-chum-repo-config" => chum = get_url(),
-                Some(n) if n == "sailfishos-chum-gui" => gui  = get_url(),
+                Some(n) if n == "sailfishos-chum-gui"         => gui  = get_url(),
                 _ => ()
               }
             }
